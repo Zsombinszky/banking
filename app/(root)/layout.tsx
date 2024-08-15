2117195a9eb976db9328ce961dbc952086bb3166
Zsombinszky <zsombi.studio@gmail.com>
2024-08-15 20:46:12 +0200

    dsfdsdfsdfsdfsdfsdfsdfdsfsfsdfsdfsdfsdfsdfsdfsdfsdfsdf

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,26 @@
 import React from "react";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav";
 
 export default function RootLayout({
                                        children,
                                    }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const loggedIn = {firstName: 'Zsombor', lastName: 'Pócs'}
     return (
-        <main>
-            SIDEBAR
-            {children}
+        <main className="flex h-screen w-full font-inter">
+            <Sidebar user={loggedIn}/>
+            <div className="size-full flex flex-col">
+                <div className="root-layout">
+                    <Image src="/icons/logo.svg" alt="logo" width={30} height={30}/>
+                    <div>
+                        <MobileNav user={loggedIn}/>
+                    </div>
+                </div>
+                {children}
+            </div>
         </main>
     );
 }
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+import CountUp from "react-countup";
+
+const AnimatedCounter = ({amount}: { amount: number }) => {
+    return (
+        <div className="w-full">
+            <CountUp duration={2.5} end={amount} prefix="$" decimal="," decimals={2}/>
+        </div>
+    )
+}
+export default AnimatedCounter
